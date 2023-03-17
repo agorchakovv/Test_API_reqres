6ac955f1e29d6c7076ba04a7425f99fa0d5e7200
agorchakovv <a.gorchakovv@gmail.com>
2023-03-17 15:46:58 +0700

    test create random user

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,25 @@
 import requests
 import pytest
+import random
+import string
 
-@pytest.fixture(scope="session")
+
+
+@pytest.fixture(scope="module")
 def api_url():
     return "https://reqres.in/api"
+
+@pytest.fixture(scope="function")
+def user_data(name_length=10):
+    letters = string.ascii_lowercase
+    name = ''.join(random.sample(letters, name_length))
+    email = name + "@demo.com"
+    data = {
+        "name": name,
+        "job": "tester",
+        "email": email
+    }
+    return data
 
 @pytest.mark.parametrize("page_num, expected_count", [(1, 6), (2, 6)])
 def test_list_users(api_url, page_num, expected_count):
@@ -13,7 +29,8 @@ def test_list_users(api_url, page_num, expected_count):
     assert len(response.json()["data"]) == expected_count, f"Print json ----- {json}"
     assert "support" in response.json(), f"Print json ----- {json}"
 
-@pytest.mark.parametrize("user_id, email, first_name, last_name", [(1, "george.bluth@reqres.in", "George", "Bluth"), (2, "janet.weaver@reqres.in", "Janet", "Weaver")])
+@pytest.mark.parametrize("user_id, email, first_name, last_name", [(1, "george.bluth@reqres.in", "George", "Bluth"),\
+                                                                   (2, "janet.weaver@reqres.in", "Janet", "Weaver")])
 def test_get_single_user(api_url, user_id, email, first_name, last_name):
     response = requests.get(f"{api_url}/users/{user_id}")
     assert response.status_code == 200
@@ -25,3 +42,11 @@ def test_get_single_user(api_url, user_id, email, first_name, last_name):
 def test_single_user_not_found(api_url, user_id):
     response = requests.get(f"{api_url}/users/{user_id}")
     assert response.status_code == 404
+
+def test_create_user(api_url, user_data):
+    data=user_data
+    response = requests.post(f"{api_url}/users", data=data)
+    assert response.status_code == 201
+    assert response.json()["name"] == user_data["name"]
+    assert response.json()["job"] == user_data["job"]
+    assert response.json()["email"] == user_data["email"]
